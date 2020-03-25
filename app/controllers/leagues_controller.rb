ab489f5c8f207cb89f9a691746697631bb1d050d
Geoffroy Goria <geoffroy2606@gmail.com>
2020-03-25 23:18:20 +0100

    controller

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -1,4 +1,5 @@
 class LeaguesController < ApplicationController
+
   def index
     @leagues = League.all
   end
@@ -8,18 +9,32 @@ class LeaguesController < ApplicationController
   end
 
   def new
-
+    @league = League.new
   end
 
   def create
+    league = League.create(league_params)
+    redirect_to league_path(league)
   end
 
   def edit
+    @league = League.find(params[:id])
   end
 
   def update
+    @league = League.find(params[:id])
+    @league.update(league_params)
   end
 
   def destroy
+    @league = League.find(params[:id])
+    @league.destroy
+    redirect_to leagues_path
   end
+
+private
+
+def league_params
+  params.require(:league).permit(:name, :stars)
+end
 end
--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,4 +1,5 @@
 class PlayersController < ApplicationController
+
   def index
     @players = Player.all
   end
@@ -8,17 +9,32 @@ class PlayersController < ApplicationController
   end
 
   def new
+    @player = Player.new
   end
 
   def create
+    Player.create(player_params)
+    redirect_to players_path
   end
 
   def edit
+    @player = Player.find(params[:id])
   end
 
   def update
+    @player = Player.find(params[:id])
+    @player.update(player_params)
   end
 
   def destroy
+    @player = Player.find(params[:id])
+    @player.destroy
+    redirect_to players_path
   end
+
+private
+
+def player_params
+  params.require(:player).permit(:name)
+end
 end
