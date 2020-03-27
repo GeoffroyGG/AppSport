38b755d53e1541718c69b3969132d8b19514d0da
Geoffroy Goria <geoffroy2606@gmail.com>
2020-03-27 11:18:24 +0100

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+League.destroy_all
+Player.destroy_all
+
 
 League.create(name: 'COD20', stars: 4)
 League.create(name: 'LOL20', stars: 5)
